796c151839ed0473dcf5b643f3cf558368f0138e
jiperezga <jiperezga@unal.edu.co>
2020-07-28 00:52:47 -0500

    Cambio tablero

--- assets/js/MathJax.js
+++ assets/js/MathJax.js
@@ -1,5 +1,5 @@
 (function () {
-  var newMathJax = 'https://raw.githubusercontent.com/jiperezga/jiperezga.github.io/master/assets/MathJax/Math/MathJax.js';
+  var newMathJax = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.6.0/MathJax.js';
   var oldMathJax = 'cdn.mathjax.org/mathjax/latest/MathJax.js';
 
   var replaceScript = function (script, src) {
